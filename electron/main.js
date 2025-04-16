57946cb0915ec2716a6a4e85bb6534132e676995
Christian Sheherlis (RIT Student) <sheherlisc@gmail.com>
2025-04-16 12:51:57 -0400

    electron demo

---
+++ electron/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const theWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: `${__dirname}/preload.js`
+    }
+  });
+
+  win.loadFile('./hosted/homePage.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+})
